f14d882b8b9fa67c0ce2b8553c75954aed505323
Harsh-sd <harshsandilya05042002@gmail.com>
2024-03-26 12:26:51 +0530

    building tree with the help og inorser and preorder

---
+++ buildtree.cpp
@@ -0,0 +1,77 @@
+// Building tree from preorder and inorder
+
+#include <iostream>
+using namespace std;
+// provided structure for the tree
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+
+public:
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    };
+};
+
+// serching the first element from preorder in inorder
+int search(int inorder[], int curr, int start, int end)
+{
+    for (int i = start; i < end; i++)
+    {
+        if (inorder[i] == curr)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+node *buildtree(int preorder[], int inorder[], int start, int end)
+{
+    static int idx = 0;
+    if (start > end)
+    {
+        return NULL;
+    }
+    int curr = preorder[idx];
+    // to traverse in the preorder
+    idx++;
+    // making the new tree
+    node *n = new node(curr);
+    // condition at last point of the tree
+    if (start == end)
+    {
+        return n;
+    }
+    // serching every element in the inorder
+    int pos = search(inorder, curr, start, end);
+    // based on its pos in inorder keepinh in the tree
+    n->left = buildtree(preorder, inorder, start, pos - 1);
+    n->right = buildtree(preorder, inorder, start, pos + 1);
+    return n;
+}
+// printing in the preorder format
+void preorderprint(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorderprint(root->left);
+    preorderprint(root->right);
+}
+int main()
+{
+    int preorder[] = {1, 2, 4, 3, 5};
+    int inorder[] = {4, 2, 1, 5, 3};
+    node *root = buildtree(preorder, inorder, 0, 4);
+    preorderprint(root);
+    return 0;
+}
+
+// result is: 1 2 4 3 5
