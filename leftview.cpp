2c4e8abb01093fe06fdf632e292dc6264bf73824
Harsh-sd <harshsandilya05042002@gmail.com>
2024-03-27 17:12:59 +0530

    seeing the tree from left side

---
+++ leftview.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+
+public:
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+void leftview(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            // we will check all the nodes and we will cout the first node because of the left view
+            node *curr = q.front();
+            q.pop();
+
+            if (i == 0)
+            {
+                cout << curr->data;
+            }
+            if (curr->left)
+            {
+                q.push(curr->left);
+            }
+            if (curr->right)
+            {
+                q.push(curr->right);
+            }
+        }
+    }
+};
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->right->left = new node(5);
+    root->right->right = new node(6);
+    root->right->left->left = new node(7);
+    leftview(root);
+}
+
+// Result is:1 2 4 7
